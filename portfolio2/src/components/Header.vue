c0bcf562f3556916b2e89993ab53660df7cdb03b
RvSpijker <robinvanspijker@gmail.com>
2025-06-04 14:16:30 +0200

    cv

--- portfolio2/src/components/Header.vue
+++ portfolio2/src/components/Header.vue
@@ -7,10 +7,8 @@
   <nav>
     <div class="nav-flex">
       <a href="#" @click.prevent="$emit('navigate', 'home')"><h2 class="navlink">Home</h2></a>
-      <a href="#" @click.prevent="$emit('navigate', 'curriculum')"
-        ><h2 class="navlink1">Curriculum Vitae</h2></a
-      >
-      <a href="#" @click.prevent="$emit('navigate', 'cv')"><h2 class="navlink2">CV</h2></a>
+      <a href="/CV_Robin2025.pdf" target="_blank"><h2 class="navlink1">Curriculum Vitae</h2></a>
+      <a href="/CV_Robin2025.pdf" target="_blank"><h2 class="navlink2">CV</h2></a>
       <a href="#" @click.prevent="$emit('navigate', 'projecten')"
         ><h2 class="navlink">Projecten</h2></a
       >
