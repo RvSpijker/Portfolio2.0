c84cc591239c51190385ace74bbeb9bab75fd417
RvSpijker <robinvanspijker@gmail.com>
2025-06-11 13:14:13 +0200

    header update

--- portfolio2/src/components/Header.vue
+++ portfolio2/src/components/Header.vue
@@ -1,11 +1,9 @@
 <template>
-  <header>
-    <a href="#" @click.prevent="$emit('navigate', 'home')">
-      <h1 class="headertext"><strong>RVSPIJKER</strong></h1>
-    </a>
-  </header>
   <nav>
     <div class="nav-flex">
+      <a href="#" @click.prevent="$emit('navigate', 'home')" class="logo">
+        <h1 class="headertext"><strong>RVSPIJKER</strong></h1>
+      </a>
       <a href="#" @click.prevent="$emit('navigate', 'home')"><h2 class="navlink">Home</h2></a>
       <a href="/CV_Robin2025.pdf" target="_blank"><h2 class="navlink1">Curriculum Vitae</h2></a>
       <a href="/CV_Robin2025.pdf" target="_blank"><h2 class="navlink2">CV</h2></a>
